6e21d157df65c33df59d04b9a7286e55911f2c6f
Ivan Ludvig <ivanludvigdev@gmail.com>
2023-02-20 22:31:07 +0300

    update

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from pynput import keyboard
 import cv2
 from deepface import DeepFace
-import math
+import numpy as np
+
+CAMERA_ID = 0
+
+data = np.recfromcsv('data.csv', delimiter=',')
+columns = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
+keys = [d[0] for d in data]
+values = [np.array(list(d)[1:]) for d in data]
 
 
 def run():
     face_analysis = None
-    camera = cv2.VideoCapture(0)
-    while face_analysis is None:
+    camera = cv2.VideoCapture(CAMERA_ID)
+    i = 0
+    while face_analysis is None and i < 10:
+        i += 1
         try:
             _, image = camera.read()
             face_analysis = DeepFace.analyze(
@@ -19,26 +28,16 @@ def run():
             pass
 
     camera.release()
-    predicted_emoji = {
-        'angry': ['😠', '😡'],
-        'disgust': ['🤢', '🤮'],
-        'fear': ['😧', '😨', '😱'],
-        'happy': ['🙂', '😄', '😆'],
-        'sad': ['🙁', '😞', '😢'],
-        'surprise': ['😮', '🤯'],
-        'neutral': ['😐']
-    }
 
-    dominant_emotion = face_analysis['dominant_emotion']
-    scale = face_analysis['emotion'][dominant_emotion]
-    predicted_emoji_list = predicted_emoji[dominant_emotion]
+    if face_analysis:
+        vector = np.array([face_analysis['emotion'][col] * 0.01 for col in columns])
+        distances = np.linalg.norm(values - vector, axis=1)
+        output = keys[np.argsort(distances)[0]]
 
-    def clamp(x):
-        return max(min(x, len(predicted_emoji_list) - 1), 0)
-    idx = clamp(math.ceil((len(predicted_emoji_list) - 1) * (scale - 50) / 50))
-
-    controller = keyboard.Controller()
-    controller.press(predicted_emoji_list[idx])
+        controller = keyboard.Controller()
+        controller.press(output)
+    else:
+        print("error")
 
 
 def for_canonical(f):
@@ -48,3 +47,4 @@ def for_canonical(f):
 hotkey = keyboard.HotKey(keyboard.HotKey.parse('<ctrl>+<alt>+f'), run)
 with keyboard.Listener(on_press=for_canonical(hotkey.press), on_release=for_canonical(hotkey.release)) as l:
     l.join()
+
